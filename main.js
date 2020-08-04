b769845cef7223c1e8e6d2726b65557c7177325a
felipedotcom <felipe.rodolfo29@gmail.com>
2020-08-04 11:59:16 -0300

    add localstorage e data

--- main.js
+++ main.js
@@ -1,27 +1,66 @@
 import BotaoConclui from './componentes/concluiTarefa.js'
 import BotaoDeleta from './componentes/deletaTarefa.js'
- 
-const criarTarefa = (evento) => {
+
+let tarefas = []
+const lista = document.querySelector('[data-list]')
+
+const handleNovoItem = (evento) => {
 
 	evento.preventDefault()
-
-	const lista = document.querySelector('[data-list]')
+	
 	const input = document.querySelector('[data-form-input]')
 	const valor = input.value
+	const date = document.querySelector('[data-form-date]')
+	const data = date.value
+
+	const dados = {
+		valor, data
+	}
+
+	lista.appendChild(criarTarefa(dados))
+	
+	tarefas.push(dados)
+
+	localStorage.setItem('tarefas', JSON.stringify(tarefas))
+	
+	input.value = " "
+}
+
+const criarTarefa = ({data, valor}) => {
 
 	const tarefa = document.createElement('li')
 	tarefa.classList.add('task')
-	const conteudo = `<p class="content">${valor}</p>`
+	
 
+	const conteudo = `<p class="content">${data}${valor}</p>
+
+	`
+	
 	tarefa.innerHTML = conteudo
 
 	tarefa.appendChild(BotaoConclui())
 	tarefa.appendChild(BotaoDeleta())
-	lista.appendChild(tarefa)
-	input.value = " "
 
+	return tarefa
 }
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
-novaTarefa.addEventListener('click', criarTarefa)
+novaTarefa.addEventListener('click', handleNovoItem)
+
+
+const carregarTarefas = () => {
+
+	const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas'))	
+
+	if(tarefasCadastradas){
+		tarefas = [...tarefasCadastradas]
+	}
+
+	tarefas.forEach(( tarefa ) => {
+		lista.appendChild(criarTarefa(tarefa))
+	})
+
+}
+
+carregarTarefas()
