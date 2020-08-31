e0c3a1458d45cca9e3aca9b55a24589845a66881
felipedotcom <felipe.rodolfo29@gmail.com>
2020-08-31 10:11:59 -0300

    add  - moment cdn  - funcao handleNovoItem, captura os dados do fomulário     - query do data-form-date     - dataFormatada com moment     - array de dados  - funcao criarTarefa, que joga os dados da handleNovoItem na tela

--- main.js
+++ main.js
@@ -1,28 +1,47 @@
 import BotaoConclui from './componentes/concluiTarefa.js'
 import BotaoDeleta from './componentes/deletaTarefa.js'
- 
-    const criarTarefa = (evento) => {
 
-    evento.preventDefault()
 
-    const lista = document.querySelector('[data-list]')
+
+const lista = document.querySelector('[data-list]')
+
+const handleNovoItem = (evento) => {
+
+	evento.preventDefault()
+
 	const input = document.querySelector('[data-form-input]')
-    const valor = input.value
+	const valor = input.value
 	const calendario = document.querySelector('[data-form-date]')
-	const data = calendario.value
-    const tarefa = document.createElement('li')
-    tarefa.classList.add('task')
-    const conteudo = `<p class="content">${valor}</p>`
+	const data = moment(calendario.value)
 
-    tarefa.innerHTML = conteudo
+	const dataFormatada = data.format('DD/MM/YYYY')
 
-    tarefa.appendChild(BotaoConclui())
-    tarefa.appendChild(BotaoDeleta())
-    lista.appendChild(tarefa)
-    input.value = " "
+	const dados = {
+		valor, dataFormatada
+	}
 
+	lista.appendChild(criarTarefa(dados))
+
+	input.value = " "
+}
+
+const criarTarefa = ({ dataFormatada, valor }) => {
+
+	const tarefa = document.createElement('li')
+	tarefa.classList.add('task')
+
+
+	const conteudo = `<p class="content">${dataFormatada} ${valor}</p>
+	`
+
+	tarefa.innerHTML = conteudo
+
+	tarefa.appendChild(BotaoConclui())
+	tarefa.appendChild(BotaoDeleta())
+
+	return tarefa
 }
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
-novaTarefa.addEventListener('click', criarTarefa)
+novaTarefa.addEventListener('click', handleNovoItem)
